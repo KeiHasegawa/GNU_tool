2d4e9fc1071dce9e5404812694634565245656d7
KeiHasegawa <kei.hasegawa.18@gmail.com>
2021-03-27 17:50:57 +0900

    fix 001

--- ptrace/test001.c
+++ ptrace/test001.c
@@ -1,5 +1,6 @@
 #include <sys/ptrace.h>
 #include <sys/user.h>
+#include <sys/wait.h>
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,28 +8,34 @@
 void print_errno(void)
 {
   switch (errno) {
-  case EBUSY:
-    puts("EBUSY");
-    return;
-  case EFAULT:
-    puts("EFAULT");
-    return;
-  case EINVAL:
-    puts("EINVAL");
-    return;
-  case EIO:
-    puts("EIO");
-    return;
-  case EPERM:
-    puts("EPERM");
-    return;
-  case ESRCH:
-    puts("ESRCH");
-    return;
+  case EBUSY:  puts("EBUSY");  return;
+  case EFAULT: puts("EFAULT"); return;
+  case EINVAL: puts("EINVAL"); return;
+  case EIO:    puts("EIO");    return;
+  case EPERM:  puts("EPERM");  return;
+  case ESRCH:  puts("ESRCH");  return;
   default:
     printf("errno = %d\n", errno);
     return;
   }
+}
+
+void print_status(int status)
+{
+  printf("status = %d\n", status);
+  if (WIFEXITED(status))
+    printf("exit %d\n", WEXITSTATUS(status));
+  if (WIFSIGNALED(status))
+    printf("signaled with signal no %d\n", WTERMSIG(status));
+  if (WIFSTOPPED(status))
+    printf("stopped with signal no %d\n", WSTOPSIG(status));
+  if (WIFCONTINUED(status))
+    puts("continued");
+}
+
+void print_regs(struct user_regs_struct* regs)
+{
+  printf("eax = 0x%x ebx = 0x%x\n", regs->eax, regs->ebx);
 }
 
 int main(int argc, char** argv)
@@ -43,11 +50,24 @@ int main(int argc, char** argv)
     print_errno();
     return 2;
   }
+  int status;
+  if (wait(&status) < 0) {
+    printf("wait() failed");
+    print_errno();
+    return 3;
+  }
+  print_status(status);
   struct user_regs_struct regs;
   if (ptrace(PTRACE_GETREGS, pid, 0, &regs) < 0) {
     printf("ptrace(PTRACE_GETREGS, %d) failed\n", pid);
     print_errno();
-    return 3; 
+    return 4; 
+  }
+  print_regs(&regs);
+  if (ptrace(PTRACE_CONT, pid, 0, SIGCONT) < 0) {
+    printf("ptrace(PTRACE_CONT, %d, 0, SIGCONT) failed\n", pid);
+    print_errno();
+    return 5; 
   }
   return 0;
 }
