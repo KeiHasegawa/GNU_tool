afd17c2af68ccece7b5673b8941117e14b4a94c1
KeiHasegawa <kei.hasegawa.18@gmail.com>
2021-04-22 16:07:52 +0900

    first bbprof

---
+++ bbprof/bbprof.cpp
@@ -0,0 +1,108 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <memory>
+#include <algorithm>
+#include <map>
+#include <cassert>
+#include <bfd.h>
+
+inline void collect(asymbol* sym, std::map<uint32_t, const char*>* info)
+{
+  asection* sec = sym->section;
+  flagword flags = sec->flags;
+  if (!(flags & SEC_CODE))
+    return;
+  uint32_t addr = sec->vma + sym->value;
+  assert(info->find(addr) == info->end());
+  (*info)[addr] = sym->name;
+}
+
+int read_prog(const char* aout, std::map<uint32_t, const char*>& info)
+{
+  using namespace std;
+  bfd* handle =  bfd_openr(aout, 0);
+  if (!handle) {
+    cerr << "bfd_openr failed" << endl;
+    return -1;
+  }
+
+  struct sweeper {
+    bfd* m_handle;
+    sweeper(bfd* handle) : m_handle{handle} {}
+    ~sweeper()
+    {
+      bfd_close(m_handle);
+    }
+  } sweeper(handle);
+
+  if (!bfd_check_format(handle, bfd_object)) {
+    cerr << "bfd_check_format failed" << endl;
+    return -2;
+  }
+
+  int upper =  bfd_get_symtab_upper_bound(handle);
+  if (upper < 0) {
+    cerr << "bfd_get_symtab_upper_bound failed" << endl;
+    return -4;
+  }
+
+  asymbol** syms = (asymbol**)(new char[upper]);
+  unique_ptr<asymbol*> sweeper2(syms);
+  int nsyms = bfd_canonicalize_symtab(handle, syms);
+  if (nsyms < 0) {
+    cerr << "bfd_canonicalize_symtab failed" << endl;
+    return -5;
+  }
+
+  for_each(&syms[0], &syms[nsyms], bind2nd(ptr_fun(collect), &info));
+
+  return 0;
+}
+
+int read_bb(const char* bbout, std::vector<uint32_t>& res)
+{
+  using namespace std;
+  ifstream ifs(bbout);
+  union {
+    uint32_t addr;
+    char c[4];
+  };
+  while (1) {
+    if (ifs.eof())
+      return 0;
+    ifs.read(&c[0], sizeof c);
+    res.push_back(addr);
+  }
+}
+
+inline void
+find_caller(uint32_t addr, const std::map<uint32_t, const char*>* info)
+{
+  using namespace std;
+  auto p = info->upper_bound(addr);
+  assert(p != info->begin());
+  --p;
+  cout << hex << addr << ',' << p->second << endl;
+}
+
+int main(int argc, char** argv)
+{
+  using namespace std;
+  if (argc != 3) {
+    cerr << "usage % " << argv[0] << " a.out bb.out" << endl;
+    return 1;
+  }
+
+  map<uint32_t, const char*> info;
+  if (read_prog(argv[1], info) < 0)
+    return 2;
+
+  vector<uint32_t> addrs;
+  if (read_bb(argv[2], addrs) < 0)
+    return 3;
+
+  for_each(begin(addrs), end(addrs), bind2nd(ptr_fun(find_caller), &info));
+
+  return 0;
+}
