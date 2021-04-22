d679594b550d4974511c04f619fdeabe6474fe0d
unknown <kei.hasegawa.18@gmail.com>
2021-04-22 19:50:40 +0900

    up pbb

--- bbprof/profile_basic_block.cpp
+++ bbprof/profile_basic_block.cpp
@@ -1,12 +1,26 @@
 #include <stdint.h>
 #include <stdio.h>
 
+#ifndef __CYGWIN__
 static FILE* fp = fopen("bb.out", "w");
+#endif // __CYGWIN__
 
 extern "C" void _profile_basic_block_()
 {
+#ifdef __CYGWIN__
+  static FILE* fp;
+  if (!fp)
+    fp = fopen("bb.out", "w");
+#endif // __CYGWIN__
+#ifdef __x86_64__
+  uint64_t addr;
+  asm("movq	8(%rbp), %rax");
+  asm("movq	%rax, -8(%rbp)");
+  fwrite(&addr, sizeof addr, 1, fp);
+#else //__x86_64__
   uint32_t addr;
   asm("movl	4(%ebp), %eax");
   asm("mov	%eax, -12(%ebp)");
   fwrite(&addr, sizeof addr, 1, fp);
+#endif // __x86_64__
 }
