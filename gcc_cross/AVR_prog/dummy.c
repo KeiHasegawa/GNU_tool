6ef0bf8407cc1de8ca64c3178be8686f5afc99ba
KeiHasegawa <kei.hasegawa.18@gmail.com>
2021-06-26 06:59:03 +0900

    up

--- gcc_cross/AVR_prog/dummy.c
+++ gcc_cross/AVR_prog/dummy.c
@@ -1,14 +1,23 @@
-int __swsetup_r()
+#include <reent.h>
+#include <stdio.h>
+
+unsigned char my_buffer[128];
+
+int __swsetup_r(struct _reent *ptr, FILE* fp)
+{
+  if (!fp->_p)
+    fp->_p = &my_buffer[0];
+  return 0;
+}
+
+int _close_r(struct _reent * ptr, int fd)
 {
   return 0;
 }
 
-void _close_r()
+_off_t _lseek_r(struct _reent * ptr, int fd, _off_t offset, int x)
 {
-}
-
-void _lseek_r()
-{
+  return offset;
 }
 
 void _malloc_r()
@@ -19,8 +28,9 @@ void _free_r()
 {
 }
 
-void _read_r()
+_ssize_t _read_r(struct _reent* ptr, int fd, void* buf, size_t size)
 {
+  return size;
 }
 
 void _write()
