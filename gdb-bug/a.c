40791669ac2e1bf384b1f27781df0ef0565bd55d
KeiHasegawa <kei.hasegawa.18@gmail.com>
2021-07-18 13:25:06 +0900

    up

---
+++ gdb-bug/a.c
@@ -0,0 +1,17 @@
+int f(int a, int b)
+{
+  switch (b) {
+  case 0: return (a >> 0) & 0xff;
+  case 1: return (a >> 8) & 0xff;
+  case 2: return (a >> 16) & 0xff;
+  case 3: return (a >> 24) & 0xff;
+  case 4: return (a >> 0) & 0xffff;
+  default: return a;
+  }
+}
+
+int main()
+{
+  f(3, 7);
+  return 0;
+}
