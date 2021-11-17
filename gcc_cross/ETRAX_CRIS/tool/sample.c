e61a2c56aaf588509bd22745fdc015fbb64c34f8
KeiHasegawa <kei.hasegawa.18@gmail.com>
2021-11-17 12:38:37 +0900

    up

---
+++ gcc_cross/ETRAX_CRIS/tool/sample.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+void f(int a, int b)
+{
+  printf("%d + %d = %d\n", a, b, a+b);
+}
+
+void g()
+{
+  printf("hello world\n"); 
+}
+
+void h(double d)
+{
+  printf("\n");
+  printf("%f\n", d);
+}
+
+void ff(long long int lli)
+{
+  printf("%lld\n", lli);
+}
+
+void ff2(long int li)
+{
+  printf("%ld\n", li);
+}
+
+void gg(const char* p, int n, double d)
+{
+  printf("%s %d %f\n", p, n, d);
+}
+
+void hh(long double ld)
+{
+  printf("%Lf\n", ld);
+}
+
+int main()
+{
+  f(5, 6);
+  g();
+  h(0.5);
+  ff(9LL);
+  ff2(10L);
+  char a[] = "hasegawa kei";
+  gg(&a[0], 7, 2.0);
+  hh(1.0L);
+  return 0;
+}
