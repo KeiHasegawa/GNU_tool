2337a1d86f17bb9a7a3441e57093da57be08a6ce
KeiHasegawa <kei.hasegawa.18@gmail.com>
2021-12-06 14:24:26 +0900

    up

--- gcc_cross/H8/mem_acc/my_cache.cpp
+++ gcc_cross/H8/mem_acc/my_cache.cpp
@@ -1,6 +1,10 @@
+#include "config.h"
+#include "bfd.h"
 #include "sim-main.h"
 
 #include <map>
+#include <vector>
+#include <algorithm>
 
 std::map<int, decoded_inst> cache;
 
@@ -18,4 +22,25 @@ extern "C" void set_cache(int pc, decoded_inst* inst)
   cache[pc] = *inst;
 }
 
+std::vector<std::pair<bfd_vma, bfd_size_type> > code_areas;
+
+bool match(std::pair<bfd_vma, bfd_size_type> p, bfd_vma x)
+{
+  auto addr = p.first;
+  auto size = p.second;
+  return addr <= x && x < addr + size;
+}
+
+extern "C" bool code_area(bfd_vma addr)
+{
+  auto p = find_if(begin(code_areas), end(code_areas),
+		   bind2nd(ptr_fun(match), addr));
+  return p != end(code_areas);
+}
+
+extern "C" void set_code_area(bfd_vma addr, bfd_size_type size)
+{
+  using namespace std;
+  code_areas.push_back(make_pair(addr, size));
+}
 
