1277d5c5e6677a0c937a98b772f2155d9c4be3bd
KeiHasegawa <kei.hasegawa.18@gmail.com>
2021-12-10 17:45:41 +0900

    up

---
+++ gcc_cross/AVR/test/test000/dummy.c
@@ -0,0 +1,29 @@
+int __swsetup_r(void)
+{
+  return 0;
+}
+
+void _close_r()
+{
+}
+
+void _lseek_r()
+{
+}
+
+void _malloc_r()
+{
+}
+
+void _free_r()
+{
+}
+
+void _read_r()
+{
+}
+
+void _write()
+{
+}
+
