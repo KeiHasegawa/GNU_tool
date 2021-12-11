a866ae6a3f12c50410096f6020c469a0113e09e3
KeiHasegawa <kei.hasegawa.18@gmail.com>
2021-12-11 13:49:47 +0900

    up

---
+++ gcc_cross/FR-V/test/test001/dummy.c
@@ -0,0 +1,36 @@
+#include <string.h>
+#include <sys/stat.h>
+
+int _fstat(int fildes, struct stat *st)
+{
+  memset(st, sizeof*st, 0);
+  st->st_mode = 0x2190;
+  return 0;
+}
+
+void* _sbrk(long inc)
+{
+#if 0  
+  extern char _heap;
+  static long delta = 0;
+  void* ret = &_heap+delta;
+  delta += inc;
+  return ret;
+#else
+  return (void*)-1;
+#endif 
+}
+
+int _isatty(int fd)
+{
+  return 1;
+}
+
+int _write(int fd, const char* buf, int len)
+{
+  return len;
+}
+
+void _close(){}
+void _lseek(){}
+void _read(){}
