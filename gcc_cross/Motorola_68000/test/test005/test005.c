ffd53bbe95e922e1a6eba7bfb7494877495288fe
KeiHasegawa <kei.hasegawa.18@gmail.com>
2021-12-12 12:34:28 +0900

    up

---
+++ gcc_cross/Motorola_68000/test/test005/test005.c
@@ -0,0 +1,16 @@
+struct S {
+  int i[4];
+};
+
+struct S f()
+{
+  struct S s = { 1, 2, 3, 4 };
+  return s;
+}
+
+
+int main()
+{
+  struct S s = f();
+  return s.i[0] + s.i[1] + s.i[2] + s.i[3];
+}
