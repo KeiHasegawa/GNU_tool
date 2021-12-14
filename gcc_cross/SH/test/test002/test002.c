acbd815ce39e80221c6e81bf767dcedaf065778f
KeiHasegawa <kei.hasegawa.18@gmail.com>
2021-12-14 10:37:34 +0900

    up

---
+++ gcc_cross/SH/test/test002/test002.c
@@ -0,0 +1,11 @@
+typedef double T;
+T f(T a0, T a1, T a2, T a3, T a4, T a5, T a6, T a7, T a8, T a9)
+{
+  return a0 + a1 + a2 + a3 + a4 + a5 + a6 + a7 + a8 + a9;
+}
+
+int main()
+{
+  f(0.5, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0);
+  return 0;
+}
