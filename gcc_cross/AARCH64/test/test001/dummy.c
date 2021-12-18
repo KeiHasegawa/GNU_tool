41a31057d14007795a6a7a42f7f6ee4b09612d62
KeiHasegawa <kei.hasegawa.18@gmail.com>
2021-12-18 11:01:05 +0900

    up

--- gcc_cross/AARCH64/test/test001/dummy.c
+++ gcc_cross/AARCH64/test/test001/dummy.c
@@ -1,29 +1,14 @@
 #include <string.h>
 #include <sys/stat.h>
 
-void _exit(int status){}
 void _close(){}
 void _lseek(){}
 void _read(){}
 
 int _fstat(int fildes, struct stat *st)
 {
-  st->st_dev = 0;
-  st->st_ino = 0;
+  memset(st, 0, sizeof*st);
   st->st_mode = 0x2190;
-  st->st_nlink = 0;
-  st->st_uid = 0;
-  st->st_gid = 0;
-  st->st_rdev = 0;
-  st->st_size = 0;
-  st->st_atim.tv_sec = 0;
-  st->st_atim.tv_nsec = 0;
-  st->st_ctim.tv_sec = 0;
-  st->st_ctim.tv_nsec = 0;
-  st->st_blksize = 0;
-  st->st_blocks = 0;
-  st->st_spare4[0] = 0;
-  st->st_spare4[1] = 0;
   return 0;
 }
 
