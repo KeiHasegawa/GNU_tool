5f6e044d5072e1d7570dc52f3c2df04bcad83b56
KeiHasegawa <kei.hasegawa.18@gmail.com>
2021-12-28 18:10:45 +0900

    up

---
+++ gcc_cross/PRU/test/test025/test.c
@@ -0,0 +1,13 @@
+int f(int a0, int a1, int a2, int a3, int a4, int a5, int a6, int a7,
+      int b0, int b1, int b2, int b3, int b4, int b5, int b6, int b7,
+      int c0, int c1)
+{
+  return a0 + a1 + a2 + a3 + a4 + a5 + a6 + a7 +
+    b0 + b1 + b2 + b3 + b4 + b5 + b6 + b7 +
+    c0 + c1;
+}
+
+int main()
+{
+  return 0;
+}
