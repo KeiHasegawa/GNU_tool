5aad91c7eca99a8c9d38496e711ffcf0ac47ff6c
KeiHasegawa <kei.hasegawa.18@gmail.com>
2021-12-30 08:53:54 +0900

    up

---
+++ gcc_cross/LatticeMico32/test/test015/test.c
@@ -0,0 +1,12 @@
+typedef long long int T;
+
+T f(int a0, T a1, int a2, int a3, T a4, int a5, int a6, int a7, T a8, int a9)
+{
+  return a0 + a1 + a2 + a3 + a4 + a5 + a6 + a7 + a8 + a9;
+}
+
+int main()
+{
+  f(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+  return 0;
+}
