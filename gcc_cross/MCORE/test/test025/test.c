faba619ad7b3d41af14c815053305d5ac4ad2de3
KeiHasegawa <kei.hasegawa.18@gmail.com>
2022-01-03 17:03:18 +0900

    up

---
+++ gcc_cross/MCORE/test/test025/test.c
@@ -0,0 +1,17 @@
+struct S {
+  int a;
+  int b;
+};
+
+struct S f(struct S x, struct S y, struct S z)
+{
+  x.a += y.a + z.a;
+  x.b += y.b + z.b;
+  return x;
+}
+
+int main()
+{
+  struct S s = f((struct S){1, 2}, (struct S){3, 4}, (struct S){5, 6});
+  return 0;
+}
