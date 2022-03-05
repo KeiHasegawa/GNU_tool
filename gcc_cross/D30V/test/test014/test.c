25f472c80b36b1d49b2e44b6515aa30be40310da
KeiHasegawa <kei.hasegawa.18@gmail.com>
2022-03-05 12:05:40 +0900

    up

---
+++ gcc_cross/D30V/test/test014/test.c
@@ -0,0 +1,18 @@
+long long f(long long a2,  long long a3,  long long a4,  long long a5,
+	    long long a6,  long long a7,  long long a8,  long long a9,
+	    long long a10, long long a11, long long a12, long long a13,
+	    long long a14, long long a15, long long a16, long long a17,
+	    long long a18, long long a19, long long a20, long long a21)
+{
+  
+  return a2 + a3 + a4 + a5
+    + a6  + a7  + a8  + a9
+    + a10 + a11 + a12 + a13
+    + a14 + a15 + a16 + a17
+    + a18 + a19 + a20 + a21;
+}
+
+int main()
+{
+  return 0;
+}
