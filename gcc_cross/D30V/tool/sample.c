10daa3f3828deeb83a3ba89251e2b67c73256df1
KeiHasegawa <kei.hasegawa.18@gmail.com>
2022-03-05 13:05:48 +0900

    up

--- gcc_cross/D30V/tool/sample.c
+++ gcc_cross/D30V/tool/sample.c
@@ -85,11 +85,18 @@ void h3(void)
   printf("%d\n", test_h3);
 }
 
-long long f4(long long a, long long b, long long c, long long d,
-	long long e, long long f, long long g, long long h,
-	long long i, long long j, long long k, long long l)
+long long f4(long long a2,  long long a3,  long long a4,  long long a5,
+	     long long a6,  long long a7,  long long a8,  long long a9,
+	     long long a10, long long a11, long long a12, long long a13,
+	     long long a14, long long a15, long long a16, long long a17,
+	     long long a18, long long a19, long long a20, long long a21)
 {
-  return a + b + c + d + e + f + g + h + i + j + k + l;
+  
+  return a2 + a3 + a4 + a5
+    + a6  + a7  + a8  + a9
+    + a10 + a11 + a12 + a13
+    + a14 + a15 + a16 + a17
+    + a18 + a19 + a20 + a21;
 }
 
 int main()
@@ -110,7 +117,8 @@ int main()
   g3();
   h3();
   printf("%lld\n",
-	 f4(1LL, 2LL, 3LL, 4LL, 5LL, 6LL, 7LL, 8LL, 9LL, 10LL, 11LL, 12LL));
+	 f4(2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
+	    19, 20, 21));
   return 0;
 }
 #if 1
