a4a0dad6d3cd50cddc6254c820727ec153b1f90e
KeiHasegawa <kei.hasegawa.18@gmail.com>
2022-03-10 13:34:20 +0900

    up

---
+++ gcc_cross/FR30/test/test014/test.c
@@ -0,0 +1,11 @@
+typedef long long int T;
+
+T f(int a1,  T a2,  int a3,  T a4, int a5,  T a6,  T a7,  T a8, T a9, T a10)
+{
+  return a1 + a2 + a3 + a4 + a5 + a6 + a7 + a8 + a9 + a10;
+}
+
+int main()
+{
+  return 0;
+}
